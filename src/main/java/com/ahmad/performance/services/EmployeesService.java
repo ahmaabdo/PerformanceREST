08a57f1e7677a6c652fa3594884509af2e138a29
AHMAD\a7maa <ahmed.abdel-gaied@appspro-me.com>
2018-04-29 21:18:47 +0200

    Delete and Update method

--- src/main/java/com/ahmad/performance/services/EmployeesService.java
+++ src/main/java/com/ahmad/performance/services/EmployeesService.java
@@ -59,7 +59,7 @@ public class EmployeesService {
 
                 employeesList.add(employee);
             }
-
+            //Close all connections
             result.close();
             statement.close();
             connection.close();
@@ -85,6 +85,7 @@ public class EmployeesService {
                 employee.setLast_name(lastName);
                 employee.setSalary(salary);
             }
+            //Close all connections
             result.close();
             statement.close();
             connection.close();
@@ -96,16 +97,63 @@ public class EmployeesService {
     }
 
     public Employee addEmployee(Employee employee) {
-        employees.put(employee.getUsername(), employee);
+        try {
+            String insertTableSQL = "INSERT INTO EMPLOYEE"
+                    + "(USERNAME, FIRST_NAME, LAST_NAME, SALARY) " + "VALUES" + "( '"
+                    + employee.getUsername() + "', '"
+                    + employee.getFirst_name() + "', '"
+                    + employee.getLast_name() + "', '"
+                    + employee.getSalary() + "' )";
+
+            result = statement.executeQuery(insertTableSQL);
+
+            //Close all connections
+            result.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+        }
+
         return employee;
     }
 
     public Employee updateEmployee(Employee employee) {
-        employees.put(employee.getUsername(), employee);
+        try {
+            String updateTableSQL = "UPDATE EMPLOYEE SET "
+                    + "FIRST_NAME = '" + employee.getFirst_name() + "', "
+                    + "LAST_NAME =  '" + employee.getLast_name() + "', "
+                    + "SALARY =  '" + employee.getSalary() + "', "
+                    + "WHERE USERNAME = '" + employee.getUsername() + "';";
+
+            result = statement.executeQuery(updateTableSQL);
+
+            //Close all connections
+            result.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+        }
+
         return employee;
     }
 
     public Employee removeEmployee(String username) {
-        return employees.remove(username);
+        Employee employee = new Employee();
+        try {
+            // int update = statement.executeUpdate("delete from ahmad.employee where username = 'ahmad123'");
+            //System.out.println(update);
+
+            result = statement.executeQuery("delete from ahmad.employee where username = " + "'" + username + "'");
+
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return employee;
     }
 }
